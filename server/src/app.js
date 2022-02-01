fe6ba507e7185494ab11748f84e522ce6d02d2b1
vishnu053 <vishnunair053@gmail.com>
2022-02-02 00:57:58 +0530

    Intial

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,22 @@ const app = express()
 const port = 3000
 
 app.get('/exec', (req, res) => {
-    console.log(req)  
-    res.send('Hello World!')
+    // req.query.c
+    console.log(execCommand())
+    res.send("output:",execCommand())
 })
+function execCommand(callback) {
+    var exec = require('child_process').exec;
+    exec("git config --global user.name",
+        function (error, stdout, stderr) {
+            if(stdout){
+           console.log('stdout: ' + stdout);
+           return stdout;
+            }
+           if (stderr !== null) {
+               console.log('exec error: ' + stderr);
+               return stderr;
+           }
+        });
+};
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
